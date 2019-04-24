eca850d64204f084e673955643c94c62bcf033a6
heichenxuan <heicx_sudo@163.com>
2019-04-24 11:49:33 +0800

    update: ba.js

--- plugins/ba.js
+++ plugins/ba.js
@@ -1,14 +1,11 @@
 
 export default ({ app }) => {
-  /*
-  ** Only run on client-side and only in production mode
-  */
  if (process.env.NODE_ENV !== 'production') return
 
   var _hmt = _hmt || [];
   (function() {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?339d01968e147d0435ab2a5f59b43338";
+    hm.src = "https://hm.baidu.com/hm.js?36734dd47df42bbcaa2095b25bb3bbb6";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
   })();
