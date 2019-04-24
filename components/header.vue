1bf35547be888a044a494451e592ed0249a35788
heichenxuan <heicx_sudo@163.com>
2019-04-24 11:44:23 +0800

    update: header

--- components/header.vue
+++ components/header.vue
@@ -2,9 +2,9 @@
 	<div class="header-topbar">
 		<div class="container clearfix">
       <router-link class="topbar-logo" to='/' tag='a'></router-link>
+      <a class="topbar-button" href="https://item.jd.com/45648416729.html" target="_blank"></a>
 			<div class="topbar-tabs">
 				<router-link v-for='(item, index) in list' :key='index' :class='{"active": item.alias.indexOf($route.name) > -1}' :to='item.link' tag='a'>{{item.name}}{{$route.test}}</router-link>
-        <a href="//www.flowclub.com/complaint" target="_blank" class="nuxt-link-active">媒体资源</a>
       </div>
 		</div>
 	</div>
@@ -20,21 +20,21 @@ export default {
 					alias: 'index',
 					link: '/'
 				},
-				{
-					name: '产品技术',
-					alias: ['tech', 'spec'],
-					link: '/tech'
-				},
-				{
-					name: '在线商城',
-					alias: 'shop',
-					link: '/shop'
-        },
-        {
-					name: '关于我们',
-					alias: 'about',
-					link: '/about'
-				},
+				// {
+				// 	name: '产品技术',
+				// 	alias: ['tech', 'spec'],
+				// 	link: '/tech'
+				// },
+				// {
+				// 	name: '在线商城',
+				// 	alias: 'shop',
+				// 	link: '/shop'
+        // },
+        // {
+				// 	name: '关于我们',
+				// 	alias: 'about',
+				// 	link: '/about'
+				// },
 			]
 		}
 	}
